1cd8fab320ae3a8188cae3130a3d9c1316ecfd27
fauzirizky <fauzirizky.id@gmail.com>
2022-01-12 06:12:31 +0700

    Authentication Value Objects

---
+++ lib/domain/core/value_validators.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+
+import 'failures.dart';
+
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  const emailRegex =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validatePassword(String input) {
+  if (input.length >= 6) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}

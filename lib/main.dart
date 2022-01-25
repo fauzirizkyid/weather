128ed3cb8b67e6703e07458c3211cd20c480d2a2
fauzirizky <fauzirizky.id@gmail.com>
2022-01-25 10:15:45 +0700

    multidex issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:code_id_storage/code_services/storage/storage.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:weather/presentation/core/app_widget.dart';
@@ -5,6 +6,7 @@ import 'package:weather/presentation/core/app_widget.dart';
 import 'injection.dart';
 
 Future<void> main() async {
+  await Storage.init();
   configureInjection(Environment.dev);
   runApp(const AppWidget());
 }
